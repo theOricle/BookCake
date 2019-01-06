8ed4d7a82316b23799902052cb99e6492c451247
matin <hsalehi91@gmail.com>
2019-01-06 01:16:20 -0500

    Added appointments in the bucket, added login by phone

--- mobile/src/main/java/com/janus/bookCake/presentation/ui/base/BaseMvpView.java
+++
@@ -1,9 +0,0 @@
-package com.janus.bookCake.presentation.ui.base;
-
-/**
- * This interface must be inherited from every other views
- * Created by Matin on 26/04/2016.
- */
-public interface BaseMvpView extends BaseView {
-
-}
