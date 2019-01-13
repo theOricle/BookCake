95dafc5922cfc16514717b208178e1b573a681a4
matin <hsalehi91@gmail.com>
2019-01-13 00:18:16 -0500

    Added appointments in the bucket, added login by phone

---
+++ mobile/src/main/java/com/janus/bookCake/presentation/ui/activities/LaunchScreen.java
@@ -0,0 +1,27 @@
+package com.janus.bookCake.presentation.ui.activities;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.cremy.greenrobotutils.library.ui.ActivityUtils;
+
+/**
+ * LaunchScreen is just here as a Branded LaunchScreen
+ * {@see https://www.google.com/design/spec/patterns/launch-screens.html}
+ */
+public class LaunchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        this.next();
+    }
+
+
+    private void next() {
+        finish();
+        ActivityUtils.cancelCloseAnimation(this);
+        OnBoardingActivity.startMe(this);
+    }
+}
